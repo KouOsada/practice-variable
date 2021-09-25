f1a50e179e4c50e0aae9fd164370a419aa847060
KouOsada <kouosada.0930@gmail.com>
2021-09-25 15:08:08 +0900

    [Add]再宣言

---
+++ sample.js
@@ -0,0 +1,20 @@
+// varによる再宣言
+// var nickname = "taro"
+// console.log(nickname)
+// var nickname = "ichiro"
+// console.log(nickname)
+
+// letによる再宣言
+// let nickname = "taro"
+// console.log(nickname)
+// let nickname = "ichiro"
+// console.log(nickname)
+// ※再宣言できない
+
+// constによる再宣言
+// const nickname = "taro"
+// console.log(nickname)
+// const nickname = "ichiro"
+// console.log(nickname)
+// ※再宣言できない
+
