340bfac486fa5541ab68b767065e1a526040479c
KouOsada <kouosada.0930@gmail.com>
2021-09-25 15:13:13 +0900

    [Add]再代入

--- sample.js
+++ sample.js
@@ -18,3 +18,22 @@
 // console.log(nickname)
 // ※再宣言できない
 
+// varによる再代入
+// var nickname = "taro"
+// console.log(nickname)
+// nickname = "jiro"
+// console.log(nickname)
+
+// letによる再代入
+// let nickname = "taro"
+// console.log(nickname)
+// nickname = "jiro"
+// console.log(nickname)
+
+// constによる再代入
+// const nickname = "taro"
+// console.log(nickname)
+// nickname = "jiro"
+// console.log(nickname)
+// ※再代入できない
+
